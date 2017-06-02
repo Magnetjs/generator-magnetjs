afaf519b2dbb2b31d9b36f58285c77de978b3370
Kieve Chua <kievechua@gmail.com>
2017-06-02 12:07:51 +0800

    update module generator

--- generators/schema/index.js
+++ generators/schema/index.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const generator_1 = require("../../utils/generator");
 module.exports = class extends generator_1.Basic {
     exec() {
-        this.writing('schema');
+        const destPath = `src/schemas/${this.name}.js`;
+        this._writing('schema.js', destPath);
     }
 };
 //# sourceMappingURL=index.js.map
